a1dc1f1769d5ebcd484658270fe9fa6be5777db2
Aaron Miller <amiller@watheia.org>
2022-01-29 03:13:24 +0000

    transpile bit components

--- apps/home/next.config.js
+++ apps/home/next.config.js
@@ -3,12 +3,12 @@ const withNx = require('@nrwl/next/plugins/with-nx');
 const withPlugins = require('next-compose-plugins');
 const withTM = require('next-transpile-modules')(
   [
-    // '@waweb/base-ui.theme.colors',
-    // '@waweb/base-ui.theme.color-definition',
-    // '@waweb/base-ui.theme.size-definition',
-    // '@waweb/base-ui.theme.shadow-definition',
-    // '@waweb/base-ui.theme.brand-definition',
-    // '@waweb/base-ui.theme.theme-provider',
+    '@waweb/base-ui.theme.colors',
+    '@waweb/base-ui.theme.color-definition',
+    '@waweb/base-ui.theme.size-definition',
+    '@waweb/base-ui.theme.shadow-definition',
+    '@waweb/base-ui.theme.brand-definition',
+    '@waweb/base-ui.theme.theme-provider',
   ],
   { debug: true }
 );

bec58d6b3368bb8c5f487e6de83e9348257bcb47
Aaron R Miller <amiller@watheia.io>
2022-02-15 09:48:39 -0800

    fix alert

--- libs/views/blog/src/lib/components/alert.tsx
+++ libs/views/blog/src/lib/components/alert.tsx
@@ -7,22 +7,20 @@ export interface AlertProps {
 }
 
 export default function Alert({ preview }: AlertProps) {
-  return (
-    !!preview && (
-      <div className={cn('bg-accent-7 border-accent-7 border-b text-gray-400')}>
-        <Container>
-          <div className="py-2 text-sm text-center">
-            This is page is showing static content.{' '}
-            <Link href="/">
-              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-              <a className="text-white underline transition-colors duration-200 hover:text-teal-500">
-                Sign-in
-              </a>
-            </Link>{' '}
-            to enter realtime mode!
-          </div>
-        </Container>
-      </div>
-    )
+  return preview ? null : (
+    <div className={cn('bg-accent-7 border-accent-7 border-b text-gray-400')}>
+      <Container>
+        <div className="py-2 text-sm text-center">
+          This is page is showing static content.{' '}
+          <Link href="/">
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a className="text-white underline transition-colors duration-200 hover:text-teal-500">
+              Sign-in
+            </a>
+          </Link>{' '}
+          to enter realtime mode!
+        </div>
+      </Container>
+    </div>
   );
 }

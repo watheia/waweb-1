17c2d2bb099fad14fcb897b69754d4b381fb5b29
Aaron R Miller <amiller@watheia.io>
2022-02-23 15:24:02 -0800

    update homepage content

--- libs/views/home/src/lib/feature-screens-section.tsx
+++ libs/views/home/src/lib/feature-screens-section.tsx
@@ -2,19 +2,21 @@ const appScreenshot =
   'https://www.datocms-assets.com/62247/1644448694-green-project-app-screenshot.jpg';
 
 const FeatureScreenSection = () => (
-  <section className="relative bg-gray-50 pt-16 sm:pt-24 lg:pt-32">
-    <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+  <section className="relative pt-16 bg-gray-50 sm:pt-24 lg:pt-32">
+    <div className="max-w-md px-4 mx-auto text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
       <div>
-        <h2 className="text-base font-semibold uppercase tracking-wider text-cyan-600">
+        <h2 className="text-base font-semibold tracking-wider uppercase text-cyan-600">
           Serverless
         </h2>
         <p className="mt-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
           No server? No problem.
         </p>
-        <p className="mx-auto mt-5 max-w-prose text-xl text-gray-500">
-          Phasellus lorem quam molestie id quisque diam aenean nulla in.
-          Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend
-          condimentum id viverra nulla.
+        <p className="mx-auto mt-5 text-xl text-gray-500 max-w-prose">
+          Deploy directly to the edge using JAMStack and a headless CMS. Enjoy
+          all the benefits of a modern micro frontend architecture using
+          interoperable components. Perform authentication and authorization
+          logic directly on the edge with stateless sessions using signed and
+          encrypted cookies to store data.
         </p>
       </div>
       <div className="mt-12 -mb-10 sm:-mb-24 lg:-mb-80">
--- libs/views/home/src/lib/hero-section.tsx
+++ libs/views/home/src/lib/hero-section.tsx
@@ -3,39 +3,39 @@ import Link from 'next/link';
 import CloudIllustration from './cloud-illustration';
 
 const HeroSection = () => (
-  <section className="bg-gray-900 pt-10 sm:pt-16 lg:overflow-hidden lg:pt-8 lg:pb-14">
+  <section className="pt-10 bg-gray-900 sm:pt-16 lg:overflow-hidden lg:pt-8 lg:pb-14">
     <div className="mx-auto max-w-7xl lg:px-8">
       <div className="lg:grid lg:grid-cols-2 lg:gap-8">
-        <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left">
+        <div className="max-w-md px-4 mx-auto sm:max-w-2xl sm:px-6 sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left">
           <div className="lg:py-24">
-            <Link href="/careers">
+            <Link href="/blog/posts/we-build-micro-frontends">
               {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-              <a className="inline-flex items-center rounded-full bg-black p-1 pr-2 text-white hover:text-gray-200 sm:text-base lg:text-sm xl:text-base">
-                <span className="rounded-full bg-gradient-to-r from-teal-500 to-cyan-600 px-3 py-0.5 text-xs font-semibold uppercase leading-5 tracking-wide text-white">
-                  We&apos;re hiring
+              <a className="inline-flex items-center p-1 pr-2 text-white bg-black rounded-full hover:text-gray-200 sm:text-base lg:text-sm xl:text-base">
+                <span className="rounded-full bg-gradient-to-r from-teal-500 to-teal-600 px-3 py-0.5 text-xs font-semibold uppercase leading-5 tracking-wide text-white">
+                  We build micro frontends!
                 </span>
-                <span className="ml-4 text-sm">Visit our careers page</span>
+                <span className="ml-4 text-sm">Read our case study.</span>
                 <ChevronRightIcon
-                  className="ml-2 h-5 w-5 text-gray-500"
+                  className="w-5 h-5 ml-2 text-gray-500"
                   aria-hidden="true"
                 />
               </a>
             </Link>
             <h1 className="mt-4 text-4xl font-extrabold tracking-tight text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
               <span className="block">A better way to</span>
-              <span className="block bg-gradient-to-r from-teal-200 to-cyan-400 bg-clip-text pb-3 text-transparent sm:pb-5">
+              <span className="block pb-3 text-transparent bg-gradient-to-r from-teal-200 to-cyan-400 bg-clip-text sm:pb-5">
                 ship web apps
               </span>
             </h1>
             <p className="text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              Lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat.
+              Let your components drive web application delivery at global
+              scale. Enjoy world-class performance, security and support every
+              step of the way.
             </p>
             <div className="mt-10 sm:mt-12">
               <form action="#" className="sm:mx-auto sm:max-w-xl lg:mx-0">
                 <div className="sm:flex">
-                  <div className="min-w-0 flex-1">
+                  <div className="flex-1 min-w-0">
                     <label htmlFor="email" className="sr-only">
                       Email address
                     </label>
@@ -43,33 +43,32 @@ const HeroSection = () => (
                       id="email"
                       type="email"
                       placeholder="Enter your email"
-                      className="block w-full rounded-md border-0 px-4 py-3 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900"
+                      className="block w-full px-4 py-3 text-base text-gray-900 placeholder-gray-500 border-0 rounded-md focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900"
                     />
                   </div>
                   <div className="mt-3 sm:mt-0 sm:ml-3">
                     <button
                       type="submit"
-                      className="block w-full rounded-md bg-gradient-to-r from-teal-500 to-cyan-600 px-4 py-3 font-medium text-white shadow hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900"
+                      className="block w-full px-4 py-3 font-medium text-white rounded-md shadow bg-gradient-to-r from-teal-500 to-teal-600 hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900"
                     >
-                      Start free trial
+                      Receive updates
                     </button>
                   </div>
                 </div>
                 <p className="mt-3 text-sm text-gray-300 sm:mt-4">
-                  Start your free 14-day trial, no credit card necessary. By
-                  providing your email, you agree to our{' '}
+                  Your privacy is important to us, read our{' '}
                   <Link href="/terms-and-conditions">
                     {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                     <a className="font-medium text-white">terms of service</a>
-                  </Link>
-                  .
+                  </Link>{' '}
+                  for more information. .
                 </p>
               </form>
             </div>
           </div>
         </div>
         <div className="mt-12 -mb-16 sm:-mb-48 lg:relative lg:m-0">
-          <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
+          <div className="max-w-md px-4 mx-auto sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
             <CloudIllustration />
           </div>
         </div>

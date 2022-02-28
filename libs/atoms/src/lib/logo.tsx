23e56c04bc65f23d21735d567e619a346b9c93b0
Aaron R Miller <amiller@watheia.io>
2022-02-28 12:31:10 -0800

    use remote image url for logo

--- libs/atoms/src/lib/logo.tsx
+++ libs/atoms/src/lib/logo.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
 import Image, { ImageProps } from 'next/image';
-
-// import logo from './assets/logo.png';
+import React from 'react';
 // import logoAlt from './assets/logo-alt.png';
-const logo = '/images/logo.png';
-const logoAlt = '/images/logo-alt.png';
+// import logo from './assets/logo.png';
+
+const logo = 'https://www.datocms-assets.com/63265/1644788635-logo.png';
+const logoAlt = 'https://www.datocms-assets.com/63265/1644788627-logo-alt.png';
 
 export interface LogoProps extends Omit<ImageProps, 'src'> {
   alternate?: boolean;
 }
 const Logo = ({
   width = 80,
-  height = 28,
+  height = 120,
   alternate = false,
   ...props
 }: LogoProps) => (
+  // eslint-disable-next-line jsx-a11y/alt-text
   <Image
     width={width}
     height={height}

c11de0089339e5bb3be513e165dce0f803fb2341
Aaron R Miller <amiller@watheia.io>
2022-02-28 19:27:44 -0800

    remove broken react hooks tests (R18-ERR)

--- libs/views/realtime/src/lib/realtime.tsx
+++ libs/views/realtime/src/lib/realtime.tsx
@@ -1,6 +1,7 @@
 import { DivProps } from '@waweb/model';
 import clsx from 'clsx';
 import styles from './realtime.module.css';
+import { styleUtils } from '@waweb/atoms';
 
 /* eslint-disable-next-line */
 export interface RealtimeProps extends DivProps {}
@@ -8,7 +9,16 @@ export interface RealtimeProps extends DivProps {}
 export function Realtime({ className, ...props }: RealtimeProps) {
   return (
     <div className={clsx(styles['root'], className)} {...props}>
-      <h1>Welcome to Realtime!</h1>
+      <h1
+        className={clsx(
+          styleUtils['appear'],
+          styleUtils['appear-third'],
+          styles['hero']
+        )}
+      >
+        Watheia
+        <br className={styleUtils['show-on-desktop']} /> Realtime
+      </h1>
     </div>
   );
 }

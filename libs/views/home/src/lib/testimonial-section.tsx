a28f3292222f5f2f270c850afca2ba29d35b71fb
Aaron R Miller <amiller@watheia.io>
2022-03-03 09:43:59 -0800

    cleanup testimonial section

--- libs/views/home/src/lib/testimonial-section.tsx
+++ libs/views/home/src/lib/testimonial-section.tsx
@@ -1,4 +1,18 @@
 import { DivProps } from '@waweb/model';
+import Image from 'next/image';
+
+const model = {
+  name: 'Judith Black',
+  title: 'CEO at PureInsights',
+  image: {
+    width: 2102,
+    height: 1401,
+    uri: 'https://www.datocms-assets.com/63265/1646325583-photo-1520333789090-1afc82db536a.jpeg'
+  },
+  content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            urna nulla vitae laoreet augue. Amet feugiat est integer dolor
+            auctor adipiscing nunc urna, sit.`
+};
 
 const TestimonialSection = (props: DivProps) => (
   <section
@@ -9,13 +23,14 @@ const TestimonialSection = (props: DivProps) => (
       <div className="relative lg:-my-8">
         <div
           aria-hidden="true"
-          className="absolute inset-x-0 top-0 bg-sidebar h-1/2 lg:hidden"
+          className="absolute inset-x-0 top-0 bg-[#111] h-1/2 lg:hidden"
         />
         <div className="max-w-md px-4 mx-auto sm:max-w-3xl sm:px-6 lg:h-full lg:p-0">
           <div className="overflow-hidden shadow-xl aspect-w-10 aspect-h-6 sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none rounded-xl lg:h-full">
-            <img
+            <Image
               className="object-cover lg:h-full lg:w-full"
-              src="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80"
+              src={model.image.uri}
+              layout="fill"
               alt=""
             />
           </div>
@@ -34,15 +49,13 @@ const TestimonialSection = (props: DivProps) => (
                 <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
               </svg>
               <p className="mt-6 text-2xl font-medium text-white">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                urna nulla vitae laoreet augue. Amet feugiat est integer dolor
-                auctor adipiscing nunc urna, sit.
+                {model.content}
               </p>
             </div>
             <footer className="mt-6">
-              <p className="text-base font-medium text-white">Judith Black</p>
+              <p className="text-base font-medium text-white">{model.name}</p>
               <p className="text-base font-medium text-teal-100">
-                CEO at PureInsights
+                {model.title}
               </p>
             </footer>
           </blockquote>
